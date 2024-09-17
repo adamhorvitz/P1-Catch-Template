2d435a129c6a7408ac69f57329cfca0e7155e4c1
adamhorvitz <149023194+adamhorvitz@users.noreply.github.com>
2024-09-17 16:34:39 -0400

    First commit

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/adamhorvitz/Documents/GitHub/P1-Catch-Template
+# Build directory: /Users/adamhorvitz/Documents/GitHub/P1-Catch-Template/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("/Users/adamhorvitz/Documents/GitHub/P1-Catch-Template/cmake-build-debug/Tests-b12d07c_include.cmake")
+subdirs("_deps/catch2-build")
